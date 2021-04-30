cbc01538c70c7fe695a061e468d391dc69df9f81
FinalJTth <FinalJTth_31@hotmail.com>
2021-05-01 05:08:16 +0700

    Fix all regex validation

--- src/gql/schema.py
+++ src/gql/schema.py
@@ -1,6 +1,8 @@
 import graphene
 import requests
 from utils.buildgql import buildgql
+import json
+import re
 
 class TestMutation(graphene.Mutation):
   class Arguments:
@@ -26,49 +28,49 @@ class Query(graphene.ObjectType):
     return True
 
   def resolve_ValidateUsername(root, info, username):
-    if (r'^[a-zA-Z]', username) is not None:
+    if (re.match(r'^[a-zA-Z]', username)) is None:
       return ValidateObject(True, 'Username must start with a letter')
-    if (r'^[a-zA-Z0-9]{0,}', username) is not None:
+    if (re.match(r'^[a-zA-Z0-9]{0,}$', username)) is None:
       return ValidateObject(True, 'Username contains implicit characters')
-    if (r'^[a-zA-Z0-9]{8,30}', username) is not None:
+    if (re.match(r'^[a-zA-Z0-9]{8,30}$', username)) is None:
       return ValidateObject(True, 'Username must be between 8 - 30 characters')
-    quert = buildgql('query', 'User', { 'username': username }, ['username'])
-    res = requests.post('https://localhost:9000/graphql', json={ query: query }, verify=False)
+    query = buildgql('query', 'User', { 'username': username }, ['username'])
+    res = requests.post('https://localhost:9000/graphql', json={ 'query': query })
     if (res.status_code == 200):
       if (len(res.json()['data']['query']) > 0):
         return ValidateObject(True, 'Username has already been registered')
     else:
-      return ValidateObject(True, 'Something went wrong while trying to check for existed user')
+      return ValidateObject(True, 'Something went wrong while checking for existed user')
     return ValidateObject(False, 'Username is valid')
   
   def resolve_ValidateEmail(root, info, email):
-    if (r'^[a-zA-Z0-9-._]$', email) is not None:
+    if (re.match(r'^[a-zA-Z0-9-._]', email)) is None:
       return ValidateObject(True, 'Email contains implicit characters')
-    if (r'^[a-zA-Z0-9-._]+@$', email) is not None:
+    if (re.match(r'^[a-zA-Z0-9-._]+@', email)) is None:
       return ValidateObject(True, 'Missing @ character')
-    if (r'^[a-zA-Z0-9-._]+@([a-zA-Z])$', email) is not None:
+    if (re.match(r'^[a-zA-Z0-9-._]+@([a-zA-Z])', email)) is None:
       return ValidateObject(True, 'Character @ must follow by proper domain name')
-    if (r'^[a-zA-Z0-9-._]+@([a-zA-Z])+[.]{1,1}$', email) is not None:
+    if (re.match(r'^[a-zA-Z0-9-._]+@([a-zA-Z])+[.]{1,1}', email)) is None:
       return ValidateObject(True, 'Missing . character')
-    if (r'^[a-zA-Z0-9-._]+@([a-zA-Z])+[.]{1,1}([a-zA-Z]){2,4}$', email) is not None:
+    if (re.match(r'^[a-zA-Z0-9-._]+@([a-zA-Z])+[.]{1,1}([a-zA-Z]){2,4}$', email)) is None:
       return ValidateObject(True, 'Invalid domain extention')
-    quert = buildgql('query', 'User', { 'email': email }, ['email'])
-    res = requests.post('https://localhost:9000/graphql', json={ query: query }, verify=False)
+    query = buildgql('query', 'User', { 'email': email }, ['email'])
+    res = requests.post('https://localhost:9000/graphql', json={ 'query': query }, verify=False)
     if (res.status_code == 200):
       if (len(res.json()['data']['query']) > 0):
         return ValidateObject(True, 'Email has already been registered')
     else:
-      return ValidateObject(True, 'Something went wrong while trying to check for existed email')
+      return ValidateObject(True, 'Something went wrong while chercking for existed email')
     return ValidateObject(False, 'Email is valid')
 
   def resolve_ValidatePassword(root, info, password):
-    if (r'^[A-Za-z0-9!"#$%&\'()*+,-./:;<>=?@[]{}\^_`~]+$', password) is not None:
+    if (re.match(r'^[A-Za-z0-9!"#$%&\'()*+,-./:;<>=?@\[\]{}\\\^_`~]', password)) is None:
       return ValidateObject(True, 'Password contains implicit characters')
-    if (r'^(?=.*[A-Z]).+$', password) is not None:
+    if (re.match(r'^(?=.*[A-Z]).+$', password)) is None:
       return ValidateObject(True, 'Password must contains at least one upper case')
-    if (r'^(?=.*[0-9]).+$', password) is not None:
+    if (re.match(r'^(?=.*[0-9]).+$', password)) is None:
       return ValidateObject(True, 'Password must contains at least one number')
-    if (r'^[A-Za-z0-9!"#$%&\'()*+,-./:;<>=?@[]{}\^_`~]{8,30}+$', password) is not None:
+    if (re.match(r'[A-Za-z0-9!"#$%&\'()*+,-./:;<>=?@\[\]{}\\\^_`~]{8,30}$', password)) is None:
       return ValidateObject(True, 'Password must be between 8 - 30 characters')
     return ValidateObject(False, 'Password is valid')
 class Mutation(graphene.ObjectType):
--- src/utils/buildgql.py
+++ src/utils/buildgql.py
@@ -17,10 +17,3 @@ def buildgql(type, resolver, parameter, returnValue=''):
   param = toGqlParamString(parameter)
   rval = f' {toGqlReturnValueString(returnValue)} ' if len(returnValue) > 0 else ''
   return f'{{ {type}: {resolver}({param}){rval}}}'
-
-print(buildgql('query', 'ValidateUsername', { 'username': 'test' }, [ 'isValid', 'message' ]))
-login = buildgql('query', 'Login', { 'username': 'test', 'password': '1234' })
-res = requests.post('https://localhost:9000/graphql', json={ 'query': login })
-print(json.dumps(res.json()))
-
-#proxies={'https': 'http://127.0.0.1:9000/graphql'}
