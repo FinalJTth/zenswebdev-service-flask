f8dd003d350fcda307d6f93a9a1e04d16dd2a5d9
FinalJTth <FinalJTth_31@hotmail.com>
2021-05-01 02:11:51 +0700

    Add buildgql

---
+++ src/utils/buildgql.py
@@ -0,0 +1,18 @@
+def toGqlParamString(obj):
+  param = ''
+  for key, value in obj.items():
+    param += f'{key}: "{value}" '
+  return param[:-1]
+
+def toGqlReturnValueString(arr):
+  rval = '{'
+  for value in arr:
+    rval += f'{value} '
+  return f'{rval[:-1]}}}'
+
+def buildgql(type, resolver, parameter, returnValue=''):
+  param = toGqlParamString(parameter)
+  rval = f' {toGqlReturnValueString(returnValue)} ' if len(returnValue) > 0 else ''
+  return f'{{ {type}: {resolver}({param}){rval}}}'
+
+print(buildgql('query', 'ValidateUsername', { 'username': 'test' }, [ 'isValid', 'message' ]))
